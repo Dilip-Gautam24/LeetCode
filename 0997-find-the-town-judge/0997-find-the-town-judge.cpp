91a0cfd073423d4a73619c3a5aece3931d978de3
Dilip-Gautam24 <65763050+Dilip-Gautam24@users.noreply.github.com>
2022-12-19 11:32:48 +0530

    Time: 522 ms (8.65%), Space: 70.8 MB (13.71%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<vector<int>> a(n+1);
-        unordered_map<int,int> mp;
+        unordered_map<int,int> mp,mpp;
         for(auto i:trust)
         {
-            a[i[0]].push_back(i[1]);
+            // a[i[0]].push_back(i[1]);
+            ++mpp[i[0]];
             ++mp[i[1]];
         }
         for(int i=1;i<=n;++i)
         {
             cout<<a[i].size()<<" "<<"\n";
             // if(a[i][0]==0) continue;
-            if(a[i].size()==0 && mp[i]==n-1) return i;
+            // if(a[i].size()==0 && mp[i]==n-1) return i;
+            if(mpp[i]==0 && mp[i]==n-1) return i;
         }
         return -1;
     }
