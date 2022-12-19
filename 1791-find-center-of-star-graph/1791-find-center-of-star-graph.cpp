849adb49c4e4d90716b9c9a6e68fa2e802af04c0
Dilip-Gautam24 <65763050+Dilip-Gautam24@users.noreply.github.com>
2022-12-19 11:43:32 +0530

    Time: 435 ms (53.86%), Space: 97.9 MB (5.82%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -3,17 +3,20 @@ public:
     int findCenter(vector<vector<int>>& edges) {
         int n=edges.size();
         cout<<n<<"\n";
+        map<int,int> mp;
         vector<vector<int>> a(n+2);
         for(auto i:edges)
         {
-            a[i[0]].push_back(i[1]);
-            a[i[1]].push_back(i[0]);
+            ++mp[i[0]];
+            ++mp[i[1]];
+            // a[i[0]].push_back(i[1]);
+            // a[i[1]].push_back(i[0]);
         }
-        for(int i=1;i<=n+1;++i)
+        for(auto i:mp)
         {
-            cout<<a[i].size()<<" "<<"\n";
+            // cout<<a[i].size()<<" "<<"\n";
             // if(a[i][0]==0) continue;
-            if(a[i].size()==n) return i;
+            if(i.second==n) return i.first;
         }
         return -1;
     }
