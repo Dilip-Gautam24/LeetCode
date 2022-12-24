d85cf61e4aada06be65797742ccaf3b3b5c36564
Dilip-Gautam24 <65763050+Dilip-Gautam24@users.noreply.github.com>
2022-12-24 12:12:01 +0530

    Time: 4 ms (44.75%), Space: 6.6 MB (31.88%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    map<long long int,long long int> mp;
-    // int call(int x)
-    // {
-    //     if(x==2) return 2;
-    //     if(mp.find(x)!=mp.end()) return mp[x];
-    //     else
-    //     {
-    //         return mp[x]=x+call(x-1);
-    //     }
-    // }
+    
     int numTilings(int n) {
+        // if(n==1) return 1;
+        // if(n==2) return 2;
+        vector<long long int> mp(n+1);
         mp[0]=1;
         mp[1]=1;
+        if(n>1)
         mp[2]=2;
         
         int sum=0,i,m=1e9+7;
         for(i=3;i<=n;++i){
-            // sum+=mp[i];
-            // if(i+1>4) sum+=1;
-            // cout<<sum<<" "<<mp[i]<<"\n";
             mp[i]=((2*mp[i-1])+(mp[i-3]))%m;
         }
-        // for(i=2;i<n;++i)
-        // {
-        //     sum+=mp[i];
-        //     cout<<mp[i]<<"\n";
-        // }
-        // mp[n]=sum;
+        // cout<<mp[1];
         return mp[n];
     }
 };
